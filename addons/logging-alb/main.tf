e42b9dff21e75e9edd7d3cbfeda7f1cf9d3317c6
Jorge Falcon <22119513+BCTBB@users.noreply.github.com>
2025-08-04 11:24:09 -0400

    Testing version 5.0 of logging-alb sub modules

--- addons/logging-alb/main.tf
+++ addons/logging-alb/main.tf
@@ -126,7 +126,7 @@ resource "aws_kms_alias" "logs_alias" {
 
 module "s3_bucket_for_logs" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.11.0"
+  version = "5.0.0"
 
   bucket = "${var.prefix}-alb-logs"
 
@@ -187,7 +187,7 @@ resource "aws_athena_database" "logs" {
 module "athena-s3-bucket" {
   count   = var.enable_athena == true ? 1 : 0
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.11.0"
+  version = "5.0.0"
 
   bucket = "${var.prefix}-alb-logs-athena"
 
