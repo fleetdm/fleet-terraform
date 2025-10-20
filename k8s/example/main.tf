86d7cee7ba55e52e5b3996ff2013525d77328073
Jorge Falcon <22119513+BCTBB@users.noreply.github.com>
2025-10-20 17:00:09 -0400

    Uncommenting database_read_replica = {}

--- k8s/example/main.tf
+++ k8s/example/main.tf
@@ -5,7 +5,7 @@ module "fleet" {
     hostname = "fleet.localhost.local"
     replicas = "3"
     image_repository = "fleetdm/fleet"
-    image_tag = "v4.72.0"
+    image_tag = "v4.75.0"
     /*
         Example:
         image_pull_secrets = [
@@ -357,7 +357,7 @@ module "fleet" {
             }   
         }
     */
-    # database_read_replica = {}
+    database_read_replica = {}
 
     cache = {
         enabled = false
