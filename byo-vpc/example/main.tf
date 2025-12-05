9971d6cdab7aae7870f308417c71b1f96c536c94
Luke Heath <luke@fleetdm.com>
2025-12-05 09:03:06 -0800

    Bump Fleet to 4.77.0 (#147)

--- byo-vpc/example/main.tf
+++ byo-vpc/example/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 }
 
 locals {
-  fleet_image = "fleetdm/fleet:v4.76.1"
+  fleet_image = "fleetdm/fleet:v4.77.0"
   domain_name = "example.com"
 }
 
--- gcp/byo-project/variables.tf
+++ gcp/byo-project/variables.tf
@@ -106,7 +106,7 @@ variable "fleet_config" {
     })))
   })
   default = {
-    image_tag              = "fleetdm/fleet:v4.76.1"
+    image_tag              = "fleetdm/fleet:v4.77.0"
     installers_bucket_name = ""
     fleet_cpu              = "1000m"
     fleet_memory           = "4096Mi"
