8c61c6fa6e627c9a163979faaaa0b6f5d7062986
Robert Fairburn <8029478+rfairburn@users.noreply.github.com>
2026-01-12 09:50:33 -0600

    Okta condiitonal access subdomain (#154)

---
+++ addons/okta-conditional-access/outputs.tf
@@ -0,0 +1,34 @@
+output "redirect_rules" {
+  value = [{
+    actions = [{
+      type        = "redirect"
+      host        = "${var.alb_config.subdomain_prefix}.#{host}"
+      path        = "/api/fleet/conditional_access/idp/sso"
+      protocol    = "HTTPS"
+      status_code = "HTTP_302"
+    }]
+    conditions = [{
+      path_patterns = ["/api/fleet/conditional_access/idp/sso"]
+    }]
+    priority = var.redirect_priority
+  }]
+}
+
+output "lb_trust_store__bucket" {
+  value = module.lb_trust_store_bucket
+}
+
+output "alb" {
+  value = merge(module.okta_mtls_alb, {
+    lb_dns_name               = module.okta_mtls_alb.dns_name
+    lb_zone_id                = module.okta_mtls_alb.zone_id
+    target_group_names        = [for k, tg in module.okta_mtls_alb.target_groups : tg.name]
+    target_group_arn_suffixes = [for k, tg in module.okta_mtls_alb.target_groups : tg.arn_suffix]
+    target_group_arns         = [for k, tg in module.okta_mtls_alb.target_groups : tg.arn]
+    lb_arn_suffix             = module.okta_mtls_alb.arn_suffix
+  })
+}
+
+output "alb_security_group" {
+  value = aws_security_group.alb
+}
---
+++ addons/okta-conditional-access/variables.tf
@@ -0,0 +1,57 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "customer_prefix" {
+  type    = string
+  default = "fleet"
+}
+
+variable "redirect_priority" {
+  description = "The priority of the redirect https_listener_rule generated in the output"
+  type        = number
+  default     = 1
+}
+
+variable "trust_store_s3_config" {
+  type = object({
+    bucket_prefix                      = optional(string, "fleet-okta-trust-store")
+    newer_noncurrent_versions          = optional(number, 5)
+    noncurrent_version_expiration_days = optional(number, 30)
+  })
+  default = {
+    bucket_prefix                      = "fleet-okta-trust-store"
+    newer_noncurrent_versions          = 5
+    noncurrent_version_expiration_days = 30
+  }
+}
+
+variable "alb_config" {
+  type = object({
+    name                       = optional(string, "fleet-okta")
+    subnets                    = list(string)
+    security_groups            = optional(list(string), [])
+    access_logs                = optional(map(string), {})
+    certificate_arn            = string
+    allowed_cidrs              = optional(list(string), ["0.0.0.0/0"])
+    allowed_ipv6_cidrs         = optional(list(string), ["::/0"])
+    egress_cidrs               = optional(list(string), ["0.0.0.0/0"])
+    egress_ipv6_cidrs          = optional(list(string), ["::/0"])
+    extra_target_groups        = optional(any, [])
+    https_listener_rules       = optional(any, [])
+    https_overrides            = optional(any, {})
+    xff_header_processing_mode = optional(string, null)
+    tls_policy                 = optional(string, "ELBSecurityPolicy-TLS13-1-2-2021-06")
+    idle_timeout               = optional(number, 60)
+    internal                   = optional(bool, false)
+    enable_deletion_protection = optional(bool, false)
+    subdomain_prefix           = optional(string, "okta")
+    trust_store = optional(any, {
+      ca_certificates_bundle_s3_key            = "ca.pem"
+      ca_certificates_bundle_s3_object_version = null
+      ca_certificates_bundle_file              = null
+      create_trust_store_revocation            = false
+      trust_store_revocation_lists             = {}
+    })
+  })
+}
