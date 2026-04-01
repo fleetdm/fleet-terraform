86e17bc83053dd4937a783c66e0b950237106ac0
Robert Fairburn <8029478+rfairburn@users.noreply.github.com>
2026-04-01 15:50:19 -0400

    Update required versions for cloud resources

--- example/main.tf
+++ example/main.tf
@@ -7,10 +7,12 @@
 # for reference.
 
 terraform {
+  required_version = ">= 1.12.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.11.0"
+      version = ">= 6.37.0"
     }
   }
 }
