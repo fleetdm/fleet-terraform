9d8df736113a6370b6c54791ae334ac8f1abc1c3
Jorge Falcon <22119513+BCTBB@users.noreply.github.com>
2026-04-08 12:30:50 -0400

    Setting blocked_encryption_types on logging-destination-splunk bucket configurations

--- addons/logging-destination-splunk/main.tf
+++ addons/logging-destination-splunk/main.tf
@@ -22,6 +22,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "splunk-failure" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "splunk-failure" {
   bucket = aws_s3_bucket.splunk-failure.bucket
   rule {
+    blocked_encryption_types = ["NONE"]
     apply_server_side_encryption_by_default {
       sse_algorithm = "aws:kms"
     }
